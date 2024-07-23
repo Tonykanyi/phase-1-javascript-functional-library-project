65c230806772d43154445ad6b304c39667f72cb2
Tony <kanyianthony79@gmail.com>
2024-07-23 17:25:31 +0300

    updated commit

--- index.js
+++ index.js
@@ -1,121 +1,103 @@
 function myEach(collection, callback) {
-    if (Array.isArray(collection)) {
-      for (let i = 0; i < collection.length; i++) {
-        callback(collection[i]);
-      }
-    } else {
-      for (let key in collection) {
-        callback(collection[key]);
-      }
+  if (Array.isArray(collection)) {
+    for (let i = 0; i < collection.length; i++) {
+      callback(collection[i], i, collection);
     }
-    return collection;
+  } else {
+    for (let key in collection) {
+      callback(collection[key], key, collection);
+    }
   }
+  return collection;
+}
+
+function myMap(collection, callback) {
+  const result = [];
+  if (Array.isArray(collection)) {
+    for (let i = 0; i < collection.length; i++) {
+      result.push(callback(collection[i], i, collection));
+    }
+  } else {
+    for (let key in collection) {
+      result.push(callback(collection[key], key, collection));
+    }
+  }
+  return result;
+}
+
+function myReduce(collection, callback, acc) {
+  const keys = !Array.isArray(collection) ? Object.keys(collection) : null;
+  const length = keys ? keys.length : collection.length;
+  let i = 0;
   
-  function myMap(collection, callback) {
-    let newArray = [];
-    if (Array.isArray(collection)) {
-      for (let i = 0; i < collection.length; i++) {
-        newArray.push(callback(collection[i]));
-      }
-    } else {
-      for (let key in collection) {
-        newArray.push(callback(collection[key], key));
-      }
-    }
-    return newArray;
-  }
-  
-  function myReduce(collection, callback, acc, context) {
-    let startIdx = 0;
-    if (acc === undefined) {
-      if (Array.isArray(collection)) {
-        acc = collection[0];
-        startIdx = 1;
-      } else {
-        const keys = Object.keys(collection);
-        acc = collection[keys[0]];
-        startIdx = 1;
-      }
-    }
-    if (Array.isArray(collection)) {
-      for (let i = startIdx; i < collection.length; i++) {
-        acc = callback.call(context, acc, collection[i], collection);
-      }
-    } else {
-      const keys = Object.keys(collection);
-      for (let i = startIdx; i < keys.length; i++) {
-        const key = keys[i];
-        acc = callback.call(context, acc, collection[key], collection);
-      }
-    }
-    return acc;
-  }
-  function myFirst(collection, n = 1) {
-    if (Array.isArray(collection)) {
-      return collection.slice(0, n);
-    } else if (typeof collection === 'object') {
-      const keys = Object.keys(collection);
-      return keys.slice(0, n).map(key => collection[key]);
-    } else {
-      return undefined;
-    }
-  }
-function myLast(collection, n = 1) {
-    if (Array.isArray(collection)) {
-      return collection.slice(-n);
-    } else {
-      const keys = Object.keys(collection);
-      const lastKeys = keys.slice(-n);
-      return lastKeys.map(key => collection[key]);
-    }
-  }
-  function myFind(collection, predicate) {
-    if (Array.isArray(collection)) {
-      for (let i = 0; i < collection.length; i++) {
-        if (predicate(collection[i])) {
-          return collection[i];
-        }
-      }
-    } else {
-      for (let key in collection) {
-        if (predicate(collection[key])) {
-          return collection[key];
-        }
-      }
-    }
-    return undefined;
-  }
-  
-  function myFilter(collection, predicate) {
-    let newArray = [];
-    if (Array.isArray(collection)) {
-      for (let i = 0; i < collection.length; i++) {
-        if (predicate(collection[i])) {
-          newArray.push(collection[i]);
-        }
-      }
-    } else {
-      for (let key in collection) {
-        if (predicate(collection[key])) {
-          newArray.push(collection[key]);
-        }
-      }
-    }
-    return newArray;
-  }
-  
-  function mySize(collection) {
-    if (Array.isArray(collection)) {
-      return collection.length;
-    } else {
-      return Object.keys(collection).length;
-    }
+  if (acc === undefined) {
+    acc = collection[keys ? keys[i++] : i++];
   }
 
-  function myKeys(obj) {
-    return Object.keys(obj);
+  for (; i < length; i++) {
+    const key = keys ? keys[i] : i;
+    acc = callback(acc, collection[key], collection);
   }
-  
-  function myValues(obj) {
-    return Object.values(obj);
+  return acc;
+}
+
+function myFind(collection, predicate) {
+  if (Array.isArray(collection)) {
+    for (let i = 0; i < collection.length; i++) {
+      if (predicate(collection[i], i, collection)) return collection[i];
+    }
+  } else {
+    for (let key in collection) {
+      if (predicate(collection[key], key, collection)) return collection[key];
+    }
   }
+  return undefined;
+}
+
+function myFilter(collection, predicate) {
+  const result = [];
+  if (Array.isArray(collection)) {
+    for (let i = 0; i < collection.length; i++) {
+      if (predicate(collection[i], i, collection)) result.push(collection[i]);
+    }
+  } else {
+    for (let key in collection) {
+      if (predicate(collection[key], key, collection)) result.push(collection[key]);
+    }
+  }
+  return result;
+}
+
+function mySize(collection) {
+  return Array.isArray(collection) ? collection.length : Object.keys(collection).length;
+}
+
+function myFirst(array, n) {
+  if (n === undefined) return array[0];
+  return array.slice(0, n);
+}
+
+function myLast(array, n) {
+  if (n === undefined) return array[array.length - 1];
+  return array.slice(-n);
+}
+
+function myKeys(object) {
+  const keys = [];
+  for (let key in object) {
+    if (object.hasOwnProperty(key)) {
+      keys.push(key);
+    }
+  }
+  return keys;
+}
+
+function myValues(object) {
+  const values = [];
+  for (let key in object) {
+    if (object.hasOwnProperty(key)) {
+      values.push(object[key]);
+    }
+  }
+  return values;
+}
